7eb514c0f10f3526414394792222950e2e9bc83e
Deise Sales <deisesalles18@gmail.com>
2024-03-20 18:46:05 -0300

    Update UserService.java

--- exercicio/src/main/java/br/com/devsuperior/exercicio/services/UserService.java
+++ exercicio/src/main/java/br/com/devsuperior/exercicio/services/UserService.java
@@ -6,6 +6,7 @@ import br.com.devsuperior.exercicio.entities.User;
 import br.com.devsuperior.exercicio.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,7 +16,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public Optional<User>  findById(Long id) {
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
 }
